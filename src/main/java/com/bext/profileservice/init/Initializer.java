abe8aa5d6b56e05fe7b5ebd322c13370a428d922
jalbertomr <jalbertomr@yahoo.com>
2022-03-30 12:20:04 -0600

    Initialize the Repository when ApplicationReadyEvent by ApplicationListener, Fill and Show the repository

---
+++ src/main/java/com/bext/profileservice/init/Initializer.java
@@ -0,0 +1,33 @@
+package com.bext.profileservice.init;
+
+import com.bext.profileservice.model.Profile;
+import com.bext.profileservice.repository.ProfileRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+@Log4j2
+@Component
+public class Initializer implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final ProfileRepository profileRepository;
+
+    public Initializer(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+    Flux<Profile> profiles = Flux
+            .just("Hugo","Paco","Luis","Daisy")
+            .map( email -> new Profile(null, email))
+            .flatMap( this.profileRepository::save);
+
+        this.profileRepository.deleteAll()
+                .thenMany( profiles)
+                .thenMany( this.profileRepository.findAll())
+                .subscribe(log::info);
+    }
+}
