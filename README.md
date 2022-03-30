5c77cbecf17042fcd835357e33bf94b2f73b8d29
jalbertomr <jalbertomr@yahoo.com>
2022-03-30 11:22:39 -0600

    model.Profile, ProfileRepository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring WebFlux profile service
+## 
+#### 
---
+++ src/main/java/com/bext/profileservice/model/Profie.java
@@ -0,0 +1,17 @@
+package com.bext.profileservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class Profie {
+    @Id
+    String id;
+    String email;
+}
